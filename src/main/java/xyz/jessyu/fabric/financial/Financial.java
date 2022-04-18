8b8bccc378b776dc08c91d355be9962745195dcd
JessYu-1011 <learningen13@gmail.com>
2022-04-18 11:28:41 +0800

    Add Coin and entity

--- src/main/java/xyz/jessyu/fabric/financial/Financial.java
+++ src/main/java/xyz/jessyu/fabric/financial/Financial.java
@@ -2,14 +2,17 @@ package xyz.jessyu.fabric.financial;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import xyz.jessyu.fabric.financial.block.CashierBlock;
+import xyz.jessyu.fabric.financial.block.CashierBlockEntity;
 import xyz.jessyu.fabric.financial.item.Coin;
 
 
@@ -17,6 +20,7 @@ public class Financial implements ModInitializer {
     public static final String MOD_ID = "financial";
     public static final CashierBlock CASHIER_BLOCK = new CashierBlock(Block.Settings.of(Material.STONE).requiresTool());
     public static final Coin COIN = new Coin(new FabricItemSettings().group(ItemGroup.MISC).maxCount(64));
+    public static BlockEntityType<CashierBlockEntity> CASHIER_BLOCK_ENTITY;
 
     @Override
     public void onInitialize() {
@@ -24,5 +28,9 @@ public class Financial implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "cashier"),
                 new BlockItem(CASHIER_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "coin"), COIN);
+        CASHIER_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE,
+                "financial:cashier_block_entity",
+                FabricBlockEntityTypeBuilder.create(CashierBlockEntity::new, CASHIER_BLOCK).build(null)
+        );
     }
 }
--- src/main/java/xyz/jessyu/fabric/financial/block/CashierBlock.java
+++ src/main/java/xyz/jessyu/fabric/financial/block/CashierBlock.java
@@ -1,8 +1,10 @@
 package xyz.jessyu.fabric.financial.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
@@ -14,22 +16,29 @@ import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
-public class CashierBlock extends Block {
+public class CashierBlock extends Block implements BlockEntityProvider {
     public CashierBlock(Settings settings){
         super(settings);
     }
-    /*
-    @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos,
-                              PlayerEntity player, Hand hand, BlockHitResult hit){
-        if(!world.isClient){
-            player.sendMessage(new LiteralText("Hello, world!"), false);
-        }
-        return ActionResult.SUCCESS;
-    }
-     */
+
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context){
         return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 1f, 1f);
     }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            player.sendMessage(new LiteralText("Hello, world!"), false);
+        }
+
+        return ActionResult.SUCCESS;
+    }
+
+    // Block entity implementation
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state){
+        return new CashierBlockEntity(pos, state);
+    }
+
 }
